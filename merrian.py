b245e8da66ce8c8300d7b1efdb874f8a9beae1b8
Anna Vahtera <anna@vahtera.org>
2025-03-01 10:56:09 +0200

    Bugfixes.

--- merrian.py
+++ merrian.py
@@ -39,16 +39,15 @@ class Language:
                     else:
                         WORDS = [temp_string.capitalize() for temp_string in getattr(glyph, part_of_speech).split("/")]
                         return f"\n {BOLD}{", ".join(WORDS)}{ENDC} = {BOLD}{BLUE}{glyph.abstract.upper()}{ENDC}:{GREEN}{BOLD}{part_of_speech.capitalize()}{ENDC}"
-                else:
-                    return f"\n {BOLD}{RED}Error{ENDC}: Word not found in database. Make sure of spelling. You wrote [{BOLD}{YELLOW}word{ENDC}]\n"
+                return f"\n {BOLD}{RED}Error{ENDC}: Word not found in database. Make sure of spelling. You wrote [{BOLD}{YELLOW}word{ENDC}]\n"
+                
     @staticmethod
     def glyphsearch(A: str, P: str): # method to search with "glyph, part"
         for glyph in Language.glyphs:
             if A in getattr(glyph, 'abstract').split("/"):
                 ANSWER = getattr(glyph, P)
                 return f"[{BOLD}{BLUE}{getattr(glyph, 'abstract').upper()}{ENDC}:{BOLD}{GREEN}{P.capitalize()}{ENDC}] {BOLD}{WHITE}{ANSWER.capitalize()}{ENDC}"
-            else:
-                return f"\n {BOLD}{RED}Error{ENDC}: Word not found in database. Make sure of spelling. You wrote [{BOLD}{YELLOW}{A}, {P}{ENDC}]\n"
+        return f"\n {BOLD}{RED}Error{ENDC}: Word not found in database. Make sure of spelling. You wrote [{BOLD}{YELLOW}{A}, {P}{ENDC}]\n"
 
 MERRIAN = Language() # Define Merrian as the language
 
