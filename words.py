62d30fd88077986e89b1ca6779779ffb94e6e0af
Anna Vahtera <anna@vahtera.org>
2025-03-02 07:17:02 +0200

    Removing file that was included accidentally.

--- words.py
+++
@@ -1,9 +0,0 @@
-from libAnna.colors import *
-def init(dict):
-    global list
-    list = {
-        "river": f"[{CYAN}long{ENDC} ({dict.get_glyph("large")} + {dict.get_glyph("distance")})]-[{dict.simpleglyphsearch('wet', 'noun')}]",
-        "doctor": f"[{dict.simpleglyphsearch('labor', 'doer')}]-[{dict.get_glyph("life")}]",
-        "mountain": f"[{dict.get_glyph("large")} + {dict.simpleglyphsearch('earth', 'noun')}]",
-        "cook": f"[{dict.simpleglyphsearch('heat', 'verb')}]-[{dict.simpleglyphsearch('flora', 'noun')}]",
-    }
